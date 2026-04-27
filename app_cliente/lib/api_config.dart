c15ced7677137070c11f671f343ae3888ebd55c1
Andres Carrasco <melquicedec125@gmail.com>
2026-04-27 18:55:51 -0400

    Configure Flutter app to consume from Render backend

--- app_cliente/lib/api_config.dart
+++ app_cliente/lib/api_config.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/foundation.dart';
 
 class ApiConfig {
+  // Cambiar a 'true' para usar localhost (desarrollo local)
+  // Cambiar a 'false' para usar Render (producción)
+  static const bool useLocalhost = false;
+  
+  static const String renderUrl = 'https://p1-emergencia.onrender.com';
+  static const String localhostUrl = 'http://localhost:8000';
+  
   static String get baseUrl {
-    if (kIsWeb) {
-      return 'http://localhost:8000';
-    }
+    if (useLocalhost) {
+      if (kIsWeb) {
+        return localhostUrl;
+      }
 
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return 'http://10.0.2.2:8000';
-      case TargetPlatform.iOS:
-      case TargetPlatform.macOS:
-      case TargetPlatform.windows:
-      case TargetPlatform.linux:
-      case TargetPlatform.fuchsia:
-        return 'http://localhost:8000';
+      switch (defaultTargetPlatform) {
+        case TargetPlatform.android:
+          return 'http://10.0.2.2:8000';
+        case TargetPlatform.iOS:
+        case TargetPlatform.macOS:
+        case TargetPlatform.windows:
+        case TargetPlatform.linux:
+        case TargetPlatform.fuchsia:
+          return localhostUrl;
+      }
+    } else {
+      // Usar Render en producción
+      return renderUrl;
     }
   }
 }
